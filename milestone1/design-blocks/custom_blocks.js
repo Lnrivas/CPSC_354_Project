f4df2d2100119326c0f3fe69ec7af4cad0f91110
iorlovic <orlovic@chapman.edu>
2023-10-08 17:04:04 -0700

    Readded workout block with append field

--- milestone1/design-blocks/custom_blocks.js
+++ milestone1/design-blocks/custom_blocks.js
@@ -1,3 +1,17 @@
+Blockly.Blocks['workout'] = {
+  init: function() {
+      this.appendDummyInput()
+          .appendField("Workout")
+          .appendField(new Blockly.FieldTextInput("Workout Name"), "WORKOUT_NAME");
+      this.appendStatementInput("EXERCISES")
+          .setCheck("exp");
+      this.setColour(130);
+      this.setTooltip("");
+      this.setHelpUrl("");
+      this.setPreviousStatement(true);  // Allow blocks to be stacked on top
+      this.setNextStatement(true);  // Allow blocks to be stacked below
+  }
+};
 
 Blockly.Blocks['exercise'] = {
   init: function() {
@@ -15,6 +29,8 @@ Blockly.Blocks['exercise'] = {
     this.setColour(330);
     this.setTooltip("");
     this.setHelpUrl("");
+    this.setPreviousStatement(true);  // Allow blocks to be stacked on top
+    this.setNextStatement(true);  // Allow blocks to be stacked below
   }
 };
 
@@ -54,18 +70,22 @@ Blockly.Blocks['rest'] = {
   }
 };
 
+Blockly.JavaScript.forBlock['workout'] = function(block) {
+  var workoutName = block.getFieldValue('WORKOUT_NAME');
+  var exercises = Blockly.JavaScript.statementToCode(block, 'EXERCISES');
+  var code = 'console.log("' + workoutName + ' workout consists of ' + exercises.split('\n').filter(Boolean).join(', ') + '.");\n';
+  return code;
+};
+
 Blockly.JavaScript.forBlock['exercise'] = function(block) {
   var exerciseName = block.getFieldValue('EXERCISE_NAME');
   var expr1_code = Blockly.JavaScript.valueToCode(block, 'EXPR1', Blockly.JavaScript.ORDER_NONE);
   var expr2_code = Blockly.JavaScript.valueToCode(block, 'EXPR2', Blockly.JavaScript.ORDER_NONE);
   var expr3_code = Blockly.JavaScript.valueToCode(block, 'EXPR3', Blockly.JavaScript.ORDER_NONE);
-  // var code = "console.log(\"Your exercise consists of " + expr1_code + " sets of " + expr2_code +
-  // " " + exerciseName + " with " + expr3_code + " minutes of rest time in between.\")";
-  // try without console log
-  var code = "\"Your exercise consists of " + expr1_code + " sets of " + expr2_code +
-  " " + exerciseName + " with " + expr3_code + " minutes of rest time in between.\"";
-return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+  var code = exerciseName + ' (' + expr1_code + ' sets, ' + expr2_code + ' reps, ' + expr3_code + ' minutes rest)';
+  return code + '\n';
 };
+
 
 Blockly.JavaScript.forBlock['sets'] = function(block) {
   var var_name = block.getFieldValue('NUM');
